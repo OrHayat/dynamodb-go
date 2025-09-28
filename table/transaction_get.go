7d30370cde1cc47af1205688eac8f378f935f6cb
Or Hayat <orhayatb@gmail.com>
2025-09-28 19:47:22 +0300

    transactionGet

--- table/transaction_get.go
+++ table/transaction_get.go
@@ -1,27 +1,108 @@
 package table
 
-// import (
-// 	"context"
+import (
+	"context"
 
-// 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-// 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-// )
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/orhayat/dynamodb-go/serializer"
+)
 
-// func prepareGetTransactionRequest() (*dynamodb.TransactGetItemsInput, error) {
-// 	request := &dynamodb.TransactGetItemsInput{
-// 		TransactItems: []types.TransactGetItem{
-// 			{
-// 				Get: &types.Get{
-// 					Key:                      nil,
-// 					TableName:                nil,
-// 					ProjectionExpression:     nil,
-// 					ExpressionAttributeNames: nil,
-// 				},
-// 			},
-// 		},
-// 	}
-// 	return request, nil
-// }
+type TransactionGetItemClient interface {
+	TransactGetItems(ctx context.Context, params *dynamodb.TransactGetItemsInput, optFns ...func(*dynamodb.Options)) (*dynamodb.TransactGetItemsOutput, error)
+}
+type TransactionGetRequet struct {
+	Table              *TableDefinition
+	Key                Key
+	Projection         *expression.ProjectionBuilder
+	Out                any //result pointer
+	AllowItemNotExists bool
+}
+
+func prepareGetTransactionRequest(getRequests []TransactionGetRequet) (*dynamodb.TransactGetItemsInput, error) {
+	txRequests := make([]types.TransactGetItem, len(getRequests))
+	for i, req := range getRequests {
+		encodedKey, err := req.Table.getKey(req.Key)
+		if err != nil {
+			return nil, err
+		}
+		var proejction *string
+		var names map[string]string
+		if req.Projection != nil {
+			b := expression.NewBuilder().WithProjection(*req.Projection)
+			expr, err := b.Build()
+			if err != nil {
+				return nil, err
+			}
+			proejction = expr.Projection()
+			names = expr.Names()
+		}
+
+		txRequests[i] = types.TransactGetItem{
+			Get: &types.Get{
+				Key:                      encodedKey,
+				TableName:                aws.String(req.Table.Name),
+				ProjectionExpression:     proejction,
+				ExpressionAttributeNames: names,
+			},
+		}
+	}
+	request := &dynamodb.TransactGetItemsInput{
+		TransactItems:          txRequests,
+		ReturnConsumedCapacity: "",
+	}
+
+	return request, nil
+}
+
+func TransactionGetItem(
+	ctx context.Context,
+	client TransactionGetItemClient,
+	getRequests []TransactionGetRequet,
+) (err error) {
+
+	request, err := prepareGetTransactionRequest(getRequests)
+	if err != nil {
+		return err
+	}
+
+	response, err := client.TransactGetItems(ctx, request)
+	if err != nil {
+		return err
+	}
+
+	for i, resp := range response.Responses {
+		//item not found
+		if resp.Item == nil {
+			//if the request allows item not exists - continue - no error getRequests[i].Out will be nil
+			if getRequests[i].AllowItemNotExists {
+				continue
+			}
+			return &OperationError{
+				operation:   "transaction get item",
+				internalErr: ErrItemNotFound,
+				table:       getRequests[i].Table,
+				pk:          getRequests[i].Key.PK,
+				sk:          getRequests[i].Key.SK,
+			}
+		}
+
+		err = serializer.UnmarshalMap(nil, resp.Item, &getRequests[i].Out)
+		if err != nil {
+			return &OperationError{
+				operation:   "transaction get item unmarshal",
+				internalErr: err,
+				table:       getRequests[i].Table,
+				pk:          getRequests[i].Key.PK,
+				sk:          getRequests[i].Key.SK,
+			}
+		}
+	}
+
+	return err
+}
 
 // func TransactGetItems(ctx context.Context) (err error) {
 // 	c := dynamodb.Client{}
